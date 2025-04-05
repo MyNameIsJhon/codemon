bb4a3fd0601eff586bf4758d8e1108680c06d9cf
0300dbdd1b <noah.gr.addino@gmail.com>
2025-04-05 11:03:22 +0200

    updating main to match better the structure we want, I write very long messages as if it was cool

--- src/main.c
+++ src/main.c
@@ -1,25 +1,43 @@
 
 #include "raylib.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct AppContext
+{
+	int		windowHeight;
+	int		windowWidth;
+	char	windowName[256];
+} AppContext;
+
+
+AppContext	*CreateAppContext()
+{
+	AppContext *ctx = malloc(sizeof(AppContext));
+	memset(ctx, 0, sizeof(AppContext));
+	strcat(ctx->windowName, "Codemon");
+	ctx->windowWidth	= 900;
+	ctx->windowHeight	= 600;
+	return (ctx);
+}
+
+void	InitApp(AppContext *ctx)
+{
+	InitWindow(ctx->windowWidth, ctx->windowHeight, ctx->windowName);
+}
 
 int main(void)
 {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
-
-	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
+	AppContext *ctx = CreateAppContext();
+	InitApp(ctx);
 	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
-
 		ClearBackground(RAYWHITE);
-
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
 		EndDrawing();
 	}
-
 	CloseWindow();
 
 	return 0;
